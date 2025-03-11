f465a6dbc51cad08fefd7f9ad6951b84e2083d86
stark0085 <aayushvijay29@gmail.com>
2025-03-12 03:33:00 +0530

    setting up the app and adding some components

--- src/Components/All_Chats/All_Chats.jsx
+++ src/Components/All_Chats/All_Chats.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './All_Chats.css'
 
 export default function All_Chats() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const allChats = [{ name: 'this', lastmsg: 'Hello' }]
+    return (
+        <div>
+            <div className="all-chats-container">
+                Here are all the chats
+                {allChats.map(function (chat) {
+                    return <div className="all-chats-inner-container">
+                        <div className="all-chats-each-chat">
+                            <div className="all-chats-chat-name">
+                                {chat.name}
+                            </div>
+                            <div className="all-chats-lastmsg">
+                                {chat.lastmsg}
+                            </div>
+                        </div>
+                    </div>
+                })}
+            </div>
+        </div>
+    )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './Navbar.css'
 
 export default function Navbar() {
   return (
     <div>
-      this is the navbar
+      <div className="navbar-container">
+        this is the navbar
+      </div>
     </div>
   )
 }
